c41d746bf9ff8dac4a8472dc167593b83d6e32af
Liunardy <54991798+Liunardy@users.noreply.github.com>
2025-06-11 22:57:18 +1000

    test(dnscheck): add unit tests for GetCoreDNSServiceIP function

--- pkg/checker/dnscheck/dns_checker_test.go
+++ pkg/checker/dnscheck/dns_checker_test.go
@@ -1,9 +1,13 @@
 package dnscheck
 
 import (
+	"context"
 	"testing"
 
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 
 	"github.com/Azure/cluster-health-monitor/pkg/config"
 )
@@ -68,6 +72,49 @@ func TestBuildDNSChecker(t *testing.T) {
 			g := NewWithT(t)
 			checker, err := BuildDNSChecker(tc.checkerName, tc.dnsConfig)
 			tc.validateRes(g, checker, err)
+		})
+	}
+}
+
+func TestGetCoreDNSServiceIP(t *testing.T) {
+	for _, tc := range []struct {
+		name           string
+		setupClientset func() *fake.Clientset
+		validateTarget func(*WithT, DNSTarget, error)
+	}{
+		{
+			name: "Success",
+			setupClientset: func() *fake.Clientset {
+				return fake.NewSimpleClientset(&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "kube-dns",
+						Namespace: "kube-system",
+					},
+					Spec: corev1.ServiceSpec{
+						ClusterIP: "10.96.0.10",
+					},
+				})
+			},
+			validateTarget: func(g *WithT, target DNSTarget, err error) {
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(target).To(Equal(DNSTarget{IP: "10.96.0.10", Type: CoreDNSService}))
+			},
+		},
+		{
+			name: "Error when service does not exist",
+			setupClientset: func() *fake.Clientset {
+				return fake.NewSimpleClientset()
+			},
+			validateTarget: func(g *WithT, target DNSTarget, err error) {
+				g.Expect(err).To(HaveOccurred())
+				g.Expect(target).To(Equal(DNSTarget{}))
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+			target, err := GetCoreDNSServiceIP(context.Background(), tc.setupClientset())
+			tc.validateTarget(g, target, err)
 		})
 	}
 }
