63ae2270800a5d22678e2855a510c75806df233f
Liunardy <54991798+Liunardy@users.noreply.github.com>
2025-06-11 22:53:50 +1000

    feat(dnscheck): add GetCoreDNSServiceIP function

--- pkg/checker/dnscheck/dns_checker.go
+++ pkg/checker/dnscheck/dns_checker.go
@@ -5,6 +5,9 @@ import (
 	"context"
 	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+
 	"github.com/Azure/cluster-health-monitor/pkg/config"
 )
 
@@ -59,4 +62,25 @@ func (c DNSChecker) Run(ctx context.Context) error {
 
 	// TODO: Implement the DNS checking logic here
 	return fmt.Errorf("DNSChecker not implemented yet")
+}
+
+// GetCoreDNSServiceIP returns the ClusterIP of the CoreDNS service in the cluster as a DNSTarget.
+func GetCoreDNSServiceIP(ctx context.Context, clientset kubernetes.Interface) (DNSTarget, error) {
+	if clientset == nil {
+		return DNSTarget{}, fmt.Errorf("clientset cannot be nil")
+	}
+
+	service, err := clientset.CoreV1().Services(CoreDNSNamespace).Get(ctx, CoreDNSServiceName, metav1.GetOptions{})
+	if err != nil {
+		return DNSTarget{}, fmt.Errorf("failed to get CoreDNS service: %w", err)
+	}
+
+	if service.Spec.ClusterIP == "" {
+		return DNSTarget{}, fmt.Errorf("CoreDNS service has no ClusterIP")
+	}
+
+	return DNSTarget{
+		IP:   service.Spec.ClusterIP,
+		Type: CoreDNSService,
+	}, nil
 }
