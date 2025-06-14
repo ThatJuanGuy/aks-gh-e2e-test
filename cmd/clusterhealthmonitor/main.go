f1d23624890747a9163e177151f29a4a52384e74
Mitch Shao <minsha@microsoft.com>
2025-06-13 18:45:29 -0700

    register builder in main

--- cmd/clusterhealthmonitor/main.go
+++ cmd/clusterhealthmonitor/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Azure/cluster-health-monitor/pkg/checker/dnscheck"
+	"github.com/Azure/cluster-health-monitor/pkg/checker/podstartup"
+)
 
 func main() {
+	registerCheckers()
 	// TODO: Add cluster health monitor implementation
 	fmt.Println("Hello world")
+}
+
+func registerCheckers() {
+	dnscheck.Register()
+	podstartup.Register()
 }
