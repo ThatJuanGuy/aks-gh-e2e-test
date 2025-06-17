d193003de9dae62a19ee0f660b8986cc53b8ef32
Mitch Shao <minsha@microsoft.com>
2025-06-17 10:24:05 -0700

    add comments

--- pkg/types/result.go
+++ pkg/types/result.go
@@ -12,13 +12,19 @@ const (
 
 // Result represents the result of a health check.
 type Result struct {
+	// Status indicates the health status of the checker.
 	Status Status
+
+	// Detail provides additional information about the health check result if it is not healthy.
 	Detail Detail
 }
 
 // Detail provides additional information about the health check result if it is not healthy.
 type Detail struct {
-	Code    string
+	// Code is a string that represents the error code of the unhealthy check result.
+	Code string
+
+	// Message is a string that provides a human-readable message about the unhealthy result.
 	Message string
 }
 
@@ -29,7 +35,7 @@ func Healthy() Result {
 	}
 }
 
-// unhealthy is a helper function to create an unhealthy Result with a specific code and message.
+// Unhealthy is a helper function to create an unhealthy Result with a specific code and message.
 func Unhealthy(code, message string) Result {
 	return Result{
 		Status: StatusUnhealthy,
