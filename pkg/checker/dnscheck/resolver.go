f87d892a075c40ae056095e57fc396efab6c777b
Liunardy <54991798+Liunardy@users.noreply.github.com>
2025-06-18 17:12:35 +1000

    refactor(dnscheck): rename dnsSvcIP to dnsIP in lookupHost

--- pkg/checker/dnscheck/resolver.go
+++ pkg/checker/dnscheck/resolver.go
@@ -12,21 +12,21 @@ const (
 
 // resolver is an interface for DNS resolution.
 type resolver interface {
-	lookupHost(ctx context.Context, dnsSvcIP, domain string) ([]string, error)
+	lookupHost(ctx context.Context, dnsIP, domain string) ([]string, error)
 }
 
 // defaultResolver implements the resolver interface using net.Resolver.
 type defaultResolver struct {
 }
 
-func (r *defaultResolver) lookupHost(ctx context.Context, dnsSvcIP, domain string) ([]string, error) {
+func (r *defaultResolver) lookupHost(ctx context.Context, dnsIP, domain string) ([]string, error) {
 	ctx, cancel := context.WithTimeout(ctx, queryTimeout)
 	defer cancel()
 	resolver := &net.Resolver{
 		PreferGo: true,
 		Dial: func(ctx context.Context, network, _ string) (net.Conn, error) {
 			d := net.Dialer{}
-			return d.DialContext(ctx, network, net.JoinHostPort(dnsSvcIP, "53"))
+			return d.DialContext(ctx, network, net.JoinHostPort(dnsIP, "53"))
 		},
 	}
 	return resolver.LookupHost(ctx, domain)
