6ab4e84192d4bcc463be9fc34895562fbd9ffe3d
Liunardy <54991798+Liunardy@users.noreply.github.com>
2025-06-20 11:30:12 +1000

    refactor(checker): simplify checker build

--- pkg/checker/checker_test.go
+++ pkg/checker/checker_test.go
@@ -27,7 +27,7 @@ func TestRegisterCheckerAndBuildChecker(t *testing.T) {
 	testType := config.CheckerType("fake")
 	RegisterChecker(testType, fakeBuilder)
 	cfg := &config.CheckerConfig{Name: "foo", Type: testType}
-	c, err := buildChecker(cfg)
+	c, err := Build(cfg)
 	g.Expect(err).ToNot(HaveOccurred())
 	g.Expect(c).ToNot(BeNil())
 	g.Expect(c.Name()).To(Equal("foo"))
@@ -36,7 +36,7 @@ func TestRegisterCheckerAndBuildChecker(t *testing.T) {
 func TestBuildCheckerUnknownType(t *testing.T) {
 	g := NewWithT(t)
 	cfg := &config.CheckerConfig{Name: "bar", Type: "unknown"}
-	c, err := buildChecker(cfg)
+	c, err := Build(cfg)
 	g.Expect(err).To(HaveOccurred())
 	g.Expect(c).To(BeNil())
 }
@@ -46,68 +46,7 @@ func TestBuildCheckerBuilderError(t *testing.T) {
 	testType := config.CheckerType("fakeerr")
 	RegisterChecker(testType, fakeBuilder)
 	cfg := &config.CheckerConfig{Name: "fail", Type: testType}
-	c, err := buildChecker(cfg)
+	c, err := Build(cfg)
 	g.Expect(err).To(HaveOccurred())
 	g.Expect(c).To(BeNil())
-}
-
-func TestBuildCheckersFromConfig_DuplicateNames(t *testing.T) {
-	g := NewWithT(t)
-	testType := config.CheckerType("dup")
-	RegisterChecker(testType, fakeBuilder)
-	yaml := `
-checkers:
-  - name: foo
-    type: dup
-  - name: foo
-    type: dup
-`
-	checkers, err := BuildCheckersFromConfig([]byte(yaml))
-	g.Expect(err).To(HaveOccurred())
-	g.Expect(checkers).To(BeNil())
-	g.Expect(err.Error()).To(ContainSubstring("duplicate checker name"))
-}
-
-func TestBuildCheckersFromConfig_UnknownType(t *testing.T) {
-	g := NewWithT(t)
-	yaml := `
-checkers:
-  - name: foo
-    type: notype
-`
-	checkers, err := BuildCheckersFromConfig([]byte(yaml))
-	g.Expect(err).To(HaveOccurred())
-	g.Expect(checkers).To(BeNil())
-	g.Expect(err.Error()).To(ContainSubstring("unrecognized checker type"))
-}
-
-func TestBuildCheckersFromConfig_InvalidYAML(t *testing.T) {
-	g := NewWithT(t)
-	badYAML := `not: [valid, yaml`
-	checkers, err := BuildCheckersFromConfig([]byte(badYAML))
-	g.Expect(err).To(HaveOccurred())
-	g.Expect(checkers).To(BeNil())
-	g.Expect(err.Error()).To(ContainSubstring("failed to unmarshal yaml"))
-}
-
-func TestBuildCheckersFromConfig_Valid(t *testing.T) {
-	g := NewWithT(t)
-	testType := config.CheckerType("oktype")
-	RegisterChecker(testType, fakeBuilder)
-	yaml := `
-checkers:
-  - name: foo
-    type: oktype
-    interval: 10s
-    timeout: 5s
-  - name: bar
-    type: oktype
-    interval: 10s
-    timeout: 5s
-`
-	checkers, err := BuildCheckersFromConfig([]byte(yaml))
-	g.Expect(err).ToNot(HaveOccurred())
-	g.Expect(checkers).To(HaveLen(2))
-	g.Expect(checkers[0].Name()).To(Equal("foo"))
-	g.Expect(checkers[1].Name()).To(Equal("bar"))
 }
