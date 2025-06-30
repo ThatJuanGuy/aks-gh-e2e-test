06645a4811a22a6d71dcf5982f525afa190fdd99
Liunardy <54991798+Liunardy@users.noreply.github.com>
2025-06-30 15:29:59 +1000

    refactor(e2e): move deployment test to a new file

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -105,22 +105,3 @@ func afterSuiteAllProcesses() {
 }
 
 var _ = SynchronizedAfterSuite(func() {}, afterSuiteAllProcesses)
-
-var _ = Describe("Cluster health monitor", func() {
-	Context("deployment", func() {
-		It("should have the cluster health monitor pod running", func() {
-			By("Checking if the cluster health monitor deployment is available")
-			deployment, err := getClusterHealthMonitorDeployment(clientset)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(deployment.Status.AvailableReplicas).To(Equal(*deployment.Spec.Replicas))
-
-			By("Checking if the cluster health monitor pod is running")
-			pod, err := getClusterHealthMonitorPod(clientset)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(pod.Status.Phase).To(BeEquivalentTo("Running"), "Pod %s should be in Running state", pod.Name)
-			for _, containerStatus := range pod.Status.ContainerStatuses {
-				Expect(containerStatus.Ready).To(BeTrue(), "Container %s in pod %s should be ready", containerStatus.Name, pod.Name)
-			}
-		})
-	})
-})
