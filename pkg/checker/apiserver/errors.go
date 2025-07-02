a57bb7d7b6d68637be101fb5f09a8eb48aa44362
Liunardy <54991798+Liunardy@users.noreply.github.com>
2025-07-02 18:39:39 +1000

    fix(apiserver): remove unused error codes

--- pkg/checker/apiserver/errors.go
+++
@@ -1,8 +0,0 @@
-package apiserver
-
-const (
-	// This is the error code of the APIServerChecker's result.
-	errCodeCreateFailed = "create_failed"
-	errCodeGetFailed    = "get_failed"
-	errCodeDeleteFailed = "delete_failed"
-)
